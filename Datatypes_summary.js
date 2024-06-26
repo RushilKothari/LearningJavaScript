d9e8d837058cb9fcf2d165dfc341a4e55bf27af1
RushilKothari <rushilkothari25.rk@gmail.com>
2024-06-26 11:05:37 +0000

    Summary of Datatypes

---
+++ Datatypes_summary.js
@@ -0,0 +1,6 @@
+// JavaScript is a dynamically typed language 
+// Primitive Types : Call by Value 
+// Seven Main Types : String, Number, Noolean, null, undefined, Symbol
+
+// Non Primitive Types/ Reference Types :  Call by Reference
+// Three Types :  Arrays, Objects, Functions
