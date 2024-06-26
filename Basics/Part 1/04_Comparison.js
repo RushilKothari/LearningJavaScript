04184ef65d30bcb8046c654949eeeb411e514281
RushilKothari <rushilkothari25.rk@gmail.com>
2024-06-26 10:22:02 +0000

    Learnt about Comparison Operations and Standard Practices

--- Basics/Part 1/04_Comparison.js
+++ Basics/Part 1/04_Comparison.js
@@ -17,6 +17,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === Used for Strict Checking, i.e., checking value as well as datatype
 
 console.log("2" === 2);
