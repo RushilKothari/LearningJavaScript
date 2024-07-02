e7d32274f885c04246e131620202a8fc795277fd
RushilKothari <rushilkothari25.rk@gmail.com>
2024-07-02 09:59:54 +0000

    Learnt about Numbers, Math library and commonly used methods

---
+++ Basics/Part 1/06_Numbers_and_Math.js
@@ -0,0 +1,36 @@
+// Numbers
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Math and Commonly used functions
+
+// console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const start = 10
+const end = 100
+
+console.log(Math.floor(Math.random() * (end - start + 1)) + start)
+// This is the formula to generate Random numbers in a given range
